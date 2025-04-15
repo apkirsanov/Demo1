49b0b70ce696aca8b8367abb944becd364983844
Anton <antonkirsanov168@gmail.com>
2025-04-15 19:55:08 +0300

    added homework_1

---
+++ src/homework_1/Homework1_5.java
@@ -0,0 +1,12 @@
+package homework_1;
+
+public class Homework1_5 {
+
+    public static void main(String[] args) {
+        int q = 22;
+        int w = 6;
+        int a = q / w;
+        int b = q % w;
+        System.out.println(q + "/" + w + " = " + a + " и " + b + " в остатке");
+    }
+}
